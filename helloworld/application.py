1471da5176551ae754e1ab02a44f5903db4f8369
Damian <danslik@interactivebrokers.ie>
2023-03-03 16:26:41 +0000

    added openai integration

--- helloworld/application.py
+++ helloworld/application.py
@@ -3,13 +3,27 @@ import json
 from flask import Flask, Response, request
 import optparse
 import os
+import openai
 
 application = Flask(__name__)
+openai.api_key = os.getenv('OPENAI_API_KEY', None)
 
 @application.route('/completion', methods=['GET'])
 def get_completion():
     prompt = request.args.get('prompt')
-    response_data = {'Prompt': prompt, 'Env': os.getenv('MY_TEST_ENV_VAR', 'Could not find ENV variable')}
+    completion = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0.9,
+        max_tokens=250,
+        n=1 # Number of completions to generate
+    )
+    completion_text = completion.choices[0].text
+    completion_id = completion.id
+    response_data = {
+        'id': completion_id,
+        'text': completion_text
+    }
     return Response(json.dumps(response_data), mimetype='application/json', status=200)
 
 @application.route('/', methods=['GET'])
