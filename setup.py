92fb28eace3a8a736252ebdff53b81af5f3b913c
Damian <danslik@interactivebrokers.ie>
2023-03-03 18:45:20 +0000

    add requirements to pre build

--- setup.py
+++ setup.py
@@ -5,8 +5,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'flask',
-        'openai',
+        'flask'
     ],
     setup_requires=[
         'pytest-runner',
